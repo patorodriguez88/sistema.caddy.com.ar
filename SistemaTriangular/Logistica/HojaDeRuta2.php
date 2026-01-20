1864500c4b892dbc43856cf3f35e9c27666b6134
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-01-20 13:16:47 -0500

    up

--- SistemaTriangular/Logistica/HojaDeRuta2.php
+++ SistemaTriangular/Logistica/HojaDeRuta2.php
@@ -632,9 +632,9 @@
     <script src="Mapas/js/controlrecorridos.js"></script>
     <script src="Proceso/js/funciones_controlrecorridos.js"></script>
     <script src="Mapas/js/datos.js"></script>
-    <script
+    <!-- <script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFDH8-tnISZXhe9BAfWw9BS-uzCv9yhvk&region=AR&language=es-419&libraries=places&callback=initMap_order&v=weekly"
-        defer></script>
+        defer></script> -->
 
 
     <!-- SweetAlert2 CSS -->
--- SistemaTriangular/Logistica/Zonas.php
+++ SistemaTriangular/Logistica/Zonas.php
@@ -146,9 +146,15 @@
                                     <!-- Multiple Select -->
                                     <div class="col-lg-12 mt-3">
                                         <div class="selector-recorrido1 form-group">
-                                            <select id="select_rec_mapa" class="select2 form-control select2-multiple" data-toggle="select2" multiple="multiple" data-placeholder="Seleccionar Recorridos ...">
-                                                <label>Seleccionar Recorrido</label>
-                                                <select id="recorrido_m" name="recorrido_m" class="form-control" data-toggle="select2" required></select>
+                                            <label for="select_rec_mapa">Seleccionar Recorridos</label>
+
+                                            <select
+                                                id="select_rec_mapa"
+                                                name="recorridos[]"
+                                                class="select2 form-control select2-multiple"
+                                                data-toggle="select2"
+                                                multiple="multiple"
+                                                data-placeholder="Seleccionar Recorridos ...">
                                             </select>
                                         </div>
                                     </div>
