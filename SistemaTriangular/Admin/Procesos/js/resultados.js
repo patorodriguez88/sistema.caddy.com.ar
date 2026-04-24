9f9cfe08987d2310eaf720b2bf7697198bacc75b
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-04-24 16:44:06 -0300

    Update resultados.js

--- SistemaTriangular/Admin/Procesos/js/resultados.js
+++ SistemaTriangular/Admin/Procesos/js/resultados.js
@@ -152,9 +152,9 @@
           <span>${formatearMoneda(venta.NetoSinIVA)}</span>
         </div>
        <div class="d-flex justify-content-between border-bottom py-1">
-  <span class="fw-semibold">C.O.D. no facturado</span>
+  <span class="fw-semibold">C.O.D. [${formatearMoneda(venta.COD_NotInvoice)}]</span>
   <span class="text-danger fw-bold">
-    ${formatearMoneda(venta.COD_NotInvoice)}
+
     ${
       venta.SurrenderNumbers
         ? `<small class="ms-2 text-muted">Surrender: ${venta.SurrenderNumbers}</small>`
