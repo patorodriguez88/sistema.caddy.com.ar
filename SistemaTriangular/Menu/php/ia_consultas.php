4780dc3a809b5f3866d9b6fca908c0244316aaa3
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-04-28 12:47:10 -0300

    ia

--- SistemaTriangular/Menu/php/ia_consultas.php
+++ SistemaTriangular/Menu/php/ia_consultas.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
 include_once __DIR__ . "/../../Conexion/Conexioni.php";
 
 date_default_timezone_set('America/Argentina/Cordoba');
