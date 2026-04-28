ac60f49fb6d03c1251f90291f01b9052071406c8
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-04-28 12:33:11 -0300

    ia

--- SistemaTriangular/Menu/php/ia_comunes.php
+++ SistemaTriangular/Menu/php/ia_comunes.php
@@ -43,3 +43,85 @@ function dinero($valor)
 {
     return '$ ' . number_format((float)$valor, 2, ',', '.');
 }
+
+function detectarFechaConsulta($q)
+{
+    $hoy = date('Y-m-d');
+
+    if (strpos($q, 'hoy') !== false) {
+        return [$hoy, 'hoy'];
+    }
+
+    if (strpos($q, 'ayer') !== false) {
+        return [date('Y-m-d', strtotime('-1 day')), 'ayer'];
+    }
+
+    if (preg_match('/(\d{1,2})[\/\-](\d{1,2})[\/\-](\d{4})/', $q, $m)) {
+        $dia = str_pad($m[1], 2, '0', STR_PAD_LEFT);
+        $mes = str_pad($m[2], 2, '0', STR_PAD_LEFT);
+        $anio = $m[3];
+
+        return ["$anio-$mes-$dia", "$dia/$mes/$anio"];
+    }
+
+    $dias = [
+        'lunes' => 'monday',
+        'martes' => 'tuesday',
+        'miercoles' => 'wednesday',
+        'jueves' => 'thursday',
+        'viernes' => 'friday',
+        'sabado' => 'saturday',
+        'domingo' => 'sunday'
+    ];
+
+    foreach ($dias as $es => $en) {
+        if (strpos($q, $es . ' pasado') !== false) {
+            return [date('Y-m-d', strtotime("last $en")), $es . ' pasado'];
+        }
+
+        if (strpos($q, $es) !== false) {
+            if (strtolower(date('l')) === $en) {
+                return [$hoy, $es];
+            }
+
+            return [date('Y-m-d', strtotime("last $en")), $es];
+        }
+    }
+
+    return [$hoy, 'hoy'];
+}
+
+function detectarPeriodoConsulta($q)
+{
+    if (strpos($q, 'este mes') !== false || strpos($q, 'mes actual') !== false) {
+        return [date('Y-m-01'), date('Y-m-t'), 'este mes'];
+    }
+
+    if (strpos($q, 'mes pasado') !== false) {
+        return [
+            date('Y-m-01', strtotime('first day of last month')),
+            date('Y-m-t', strtotime('last day of last month')),
+            'el mes pasado'
+        ];
+    }
+
+    if (strpos($q, 'esta semana') !== false) {
+        return [
+            date('Y-m-d', strtotime('monday this week')),
+            date('Y-m-d', strtotime('sunday this week')),
+            'esta semana'
+        ];
+    }
+
+    if (strpos($q, 'semana pasada') !== false) {
+        return [
+            date('Y-m-d', strtotime('monday last week')),
+            date('Y-m-d', strtotime('sunday last week')),
+            'la semana pasada'
+        ];
+    }
+
+    list($fecha, $texto) = detectarFechaConsulta($q);
+
+    return [$fecha, $fecha, $texto];
+}
