036b6b419e382206b6a003a637d61a3aa7a956a9
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-06-03 11:56:55 -0300

    Update datosempresa.php

--- SistemaTriangular/Funciones/php/datosempresa.php
+++ SistemaTriangular/Funciones/php/datosempresa.php
@@ -1,18 +1,21 @@
 <?php
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include_once "../../Conexion/Conexioni.php";
 header('Content-Type: application/json; charset=utf-8');
 
 if (isset($_POST['Empresa'])) {
-  $sql       = "SELECT * FROM DatosEmpresa";
-  $Resultado = $mysqli->query($sql);
-  $rows      = [];
+    $sql       = "SELECT * FROM DatosEmpresa";
+    $Resultado = $mysqli->query($sql);
+    $rows      = [];
 
-  if ($Resultado) {
-      while ($row = $Resultado->fetch_array(MYSQLI_ASSOC)) {
-          $rows[] = $row;
-      }
-  }
+    if ($Resultado) {
+        while ($row = $Resultado->fetch_array(MYSQLI_ASSOC)) {
+            $rows[] = $row;
+        }
+    }
 
-  echo json_encode(['data' => $rows]);
+    echo json_encode(['data' => $rows]);
 }
