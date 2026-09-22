78476fb0d4f8f7379adb4e8d79b6dca35963d275
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-09-22 18:02:32 -0300

    fix include path tmp verif script

--- SistemaTriangular/Admin/Procesos/php/_tmp_verif_conciliacion.php
+++ SistemaTriangular/Admin/Procesos/php/_tmp_verif_conciliacion.php
@@ -1,6 +1,6 @@
 <?php
 define('ALLOW_NO_SESSION', true);
-include_once __DIR__ . "/Conexioni.php";
+include_once __DIR__ . "/../../../Conexion/Conexioni.php";
 header('Content-Type: application/json; charset=utf-8');
 $r = $mysqli->query("SHOW COLUMNS FROM Tesoreria LIKE 'UsuarioConciliado'")->fetch_assoc();
 $r2 = $mysqli->query("SHOW COLUMNS FROM Tesoreria LIKE 'idConciliacionBancaria'")->fetch_assoc();
