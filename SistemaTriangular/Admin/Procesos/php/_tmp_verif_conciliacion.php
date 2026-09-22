7cb599c8a4057a5a47596192000c707cdf76b37e
Patricio Rodriguez <prodriguez@dintersa.com.ar>
2026-09-22 18:01:49 -0300

    tmp: verif script conciliacion (se borra enseguida)

---
+++ SistemaTriangular/Admin/Procesos/php/_tmp_verif_conciliacion.php
@@ -0,0 +1,10 @@
+<?php
+define('ALLOW_NO_SESSION', true);
+include_once __DIR__ . "/Conexioni.php";
+header('Content-Type: application/json; charset=utf-8');
+$r = $mysqli->query("SHOW COLUMNS FROM Tesoreria LIKE 'UsuarioConciliado'")->fetch_assoc();
+$r2 = $mysqli->query("SHOW COLUMNS FROM Tesoreria LIKE 'idConciliacionBancaria'")->fetch_assoc();
+$r3 = $mysqli->query("DESCRIBE ConciliacionBancaria");
+$cols = [];
+while ($row = $r3->fetch_assoc()) $cols[] = $row;
+echo json_encode(['UsuarioConciliado'=>$r, 'idConciliacionBancaria'=>$r2, 'ConciliacionBancaria_cols'=>$cols], JSON_PRETTY_PRINT);
